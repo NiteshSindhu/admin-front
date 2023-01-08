5b8e7136205eadef0890390b166cadf2e14da89c
NITESH SINDHU <niteshsindhu1435@gmail.com>
2023-01-08 08:10:24 +0530

    front

--- Admin/src/ReactRedux/api.js
+++ Admin/src/ReactRedux/api.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 
 export const api = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:9000" }),
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL}),
   reducerPath: "adminApi",
   tagTypes: [
     "User",
